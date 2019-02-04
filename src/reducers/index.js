d578b8164fc8574b938339101ced9da9e1e82bc3
UchuNoKen <xen.o.fite@gmail.com>
2019-02-04 17:19:19 -0500

    Wired up reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
