2dff6c4028bb122e5a958f3decd64ac935ade7f6
UchuNoKen <xen.o.fite@gmail.com>
2019-02-04 19:43:30 -0500

    Added list items and structure.

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import SongList from "./SongList";
 
 const App = () => {
-  return <div>Song List</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
+
+// <div className="ui container grid">
+// <div className="ui row">
+//   <div className="column eight wide">
+//   </div>
+//   </div>
+// </div>
